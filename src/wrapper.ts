a13fa01f6664cb20d8304b391bd3d2e98e797ea5
Pedro Henrique <opedro3g@gmail.com>
2024-04-07 19:32:14 -0300

    chore(package.json): add @types/formidable and formidable dependencies refactor(wrapper.ts): update wrapper function to handle different content types in the request body and log the received data for debugging purposes

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,26 +1,56 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingForm } from "formidable";
+import { IncomingMessage, ServerResponse } from "node:http";
 
 export default async function wrapper(
   im: IncomingMessage,
   sr: ServerResponse,
   fn: (options: RouteOptions) => Promise<unknown> | unknown
 ) {
-  const body = im.read() ?? {};
+  let data = "";
 
-  const response = fn({ body });
+  im.on("data", (chunk) => {
+    data += chunk;
+  });
 
-  if (typeof response === "string") {
-    sr.writeHead(200, { "Content-Type": "text/plain" });
-    sr.end(response);
-  } else if (typeof response === "object") {
-    sr.writeHead(200, { "Content-Type": "application/json" });
-    sr.end(JSON.stringify(response));
-  } else {
-    sr.writeHead(500, { "Content-Type": "text/plain" });
-    sr.end("Malformed response");
-  }
+  im.on("end", async () => {
+    if (im.headers["content-type"] === "application/json") {
+      data = JSON.parse(data);
+    }
+    // else if (im.headers["content-type"].startsWith("multipart/form-data")) {
+    //   const incomingForm = new IncomingForm();
+
+    //   const a = await new Promise((resolve, reject) => {
+    //     incomingForm.parse(im, (err, fields, files) => {
+    //       if (err) {
+    //         reject(err);
+    //       } else {
+    //         resolve({ fields, files });
+    //       }
+    //     });
+    //   });
+
+    //   console.log({ a });
+    // }
+
+    console.log({ type: im.headers["content-type"] });
+
+    console.log(data);
+
+    const response = fn({ data });
+
+    if (typeof response === "string") {
+      sr.writeHead(200, { "Content-Type": "text/plain" });
+      sr.end(response);
+    } else if (typeof response === "object") {
+      sr.writeHead(200, { "Content-Type": "application/json" });
+      sr.end(JSON.stringify(response));
+    } else {
+      sr.writeHead(500, { "Content-Type": "text/plain" });
+      sr.end("Malformed response");
+    }
+  });
 }
 
 export type RouteOptions = {
-  body: unknown;
+  data: unknown;
 };
